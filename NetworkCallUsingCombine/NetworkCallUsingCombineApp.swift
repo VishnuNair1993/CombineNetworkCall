891ca1148513cfc48eab6ed232187d03f5996f8b
Vishnu Nair <vishnunair2011@gmail.com>
2025-09-27 09:58:30 +0530

    Initial Commit

---
+++ NetworkCallUsingCombine/NetworkCallUsingCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallUsingCombineApp.swift
+//  NetworkCallUsingCombine
+//
+//  Created by Vishnu M Nair on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallUsingCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
